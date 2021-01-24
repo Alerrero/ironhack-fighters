7ae67ebc81aac744f7861f0a617b8d203053a23f
Alejandro <evergreenterras@hotmail.com>
2021-01-24 11:48:41 +0100

    healthBar

--- js/app.js
+++ js/app.js
@@ -22,9 +22,17 @@ const ironhackFighters = {
         this.ctx = this.canvasDom.getContext('2d')
         this.setDimensions()
         this.lifeBars.push(new LifeBar(this.ctx, this.canvasSize, 100, 'player1'))
+        this.lifeBars.push(new LifeBar(this.ctx, this.canvasSize, 100, 'player2'))
+        this.lifeBars[0].drawFramework()
+        this.lifeBars[1].drawFramework()
         this.lifeBars[0].setHealthBarPos()
+        this.lifeBars[1].setHealthBarPos()
         this.lifeBars[0].fillHealthBar()
+        this.lifeBars[1].fillHealthBar()
+    
+       // this.lifeBars[0].fillHealthBar()
         console.log(this.lifeBars[0])
+        
     },
 
     render() {
--- js/lifeBar.js
+++ js/lifeBar.js
@@ -4,29 +4,35 @@ class LifeBar {
         this.canvasSize = canvasSize
         this.playerHealth = playerHealth
         this.playerType = playerType
-        this.healthBarPos = { x: 0, y: 200 }
+        this.healthBarPos = { x: 0, y: 20 }
         this.initialWidth = 0
         this.healthBarSize = { w: this.playerHealth, h: 50 }
-        this.frameworkWidth = 200
+        this.frameworkWidth = this.canvasSize.w / 2 -40
         this.frameworkPosition = {
-            x: 220,
-            y: 220
+            x: 0,
+            y: 20
         }
     }
 
     setHealthBarPos() {
-        this.playerType === 'player1' ? this.healthBarPos.x = this.frameworkPosition.x + this.frameworkWidth - this.healthBarSize.w : this.frameworkPosition.x
+        this.playerType === 'player1' ? this.healthBarPos.x = this.frameworkPosition.x + this.frameworkWidth - this.healthBarSize.w : this.healthBarPos.x = this.frameworkPosition.x
+        console.log(this.healthBarPos.x, this.playerType, this.frameworkPosition.x)
+
     }
 
     drawFramework() {
-        this.ctx.draw()
+       this.ctx.fillStyle = 'red'
+        if (this.playerType === 'player1') {
+            this.frameworkPosition.x = 20
+        } else { this.frameworkPosition.x = this.canvasSize.w / 2 + 20     
+        } this.ctx.fillRect( this.frameworkPosition.x, this.frameworkPosition.y, this.frameworkWidth, 50)
     }
 
     fillHealthBar() {
         // this.playerType === 'player1' ? this.ctx.fillRect(this.healthBarPos.x, this.healthBarPos.y, this.healthBarSize.w, this.healthBarSize.h) : this.ctx.fillRect(this.healthBarPos.x + this.initialWidth - this.healthBarSize.w, this.healthBarPos.y, this.healthBarSize.w, this.healthBarSize.h)
         this.ctx.fillStyle = 'green'
-            //this.ctx.fillRect(this.posX, this.posY, this.healthBarSize.w, this.healthBarSize.h)
-        this.ctx.fillRect(0, 0, 300, 200)
+        this.ctx.fillRect(this.healthBarPos.x, this.healthBarPos.y, this.healthBarSize.w, this.healthBarSize.h)
+       
     }
 
     setHealthBarWidth() {
