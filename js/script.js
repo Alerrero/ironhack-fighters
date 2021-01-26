1c8ee9b9ac7d1a0d7158331c787790fc9ce78e33
Alejandro <evergreenterras@hotmail.com>
2021-01-26 17:21:42 +0100

    audio

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 window.onload = () => {
     document.getElementById('start-button').onclick = function() {
         document.getElementById('start-button').disabled = true
+        const audio = document.querySelector("#audio-pelea1");
         startGame()
-    }
+        console.log (audio);
+        setTimeout(() =>{
+        audio.play();  
+       }, 2000);
+       };
+
+    
     document.getElementById('restart-button').onclick = () => {
         restartGame();
     };
