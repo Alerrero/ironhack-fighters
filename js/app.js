36572c82a4ecd82ccfb1bcdd98a23f4459587b75
Alejandro <evergreenterras@hotmail.com>
2021-01-27 15:38:10 +0100

    slection players

--- js/app.js
+++ js/app.js
@@ -219,4 +219,44 @@ const ironhackFighters = {
     clearScreen() {
         this.ctx.clearRect(0, 0, this.canvasSize.w, this.canvasSize.h)
     },
+
+
 }
+window.onload = () => {
+    document.getElementById('start-button').onclick = function() {
+        document.getElementById('start-button').disabled = true
+        const audio = document.querySelector("#audio-pelea1");
+        audio.play();
+        audio.volume= 0.2;  
+        startGame()
+        };
+
+    
+    document.getElementById('restart-button').onclick = () => {
+        restartGame();
+    };
+    document.getElementById('Green').onclick = function() {
+        document.querySelector('.character-selection').style.display = 'none'
+        ironhackFighters.setPlayer1Character('Green')
+    };
+    document.getElementById('Red').onclick = function() {
+        document.querySelector('.character-selection').style.display = 'none'
+        ironhackFighters.setPlayer1Character('Red')
+    }
+
+}
+
+function startGame() {
+    ironhackFighters.init('canvas')
+    ironhackFighters.render()
+}
+
+function restartGame() {
+    document.querySelector('.end-game').style.display = 'none'
+    document.getElementById('start-button').disabled = true
+
+    ironhackFighters.restart()
+    ironhackFighters.render()
+
+}
+
